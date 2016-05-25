6975e1d17d61da97890451601d6f5ea5d1a61ecc
iamericmaxwell <iamericmaxwell@gmail.com>
2016-05-24 18:06:28 -0700

    modified template resource to pass more variables into template

--- recipes/conf.rb
+++ recipes/conf.rb
@@ -4,13 +4,13 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-# grab the data from data_bags
+#grab the data from data_bags
 nodes = data_bag_item(
-    "slurm-#{node['slurm-wlm']['configs']['ClusterName']}",
+    "slurm-#{node['slurm-wlm']['config']['ClusterName']}",
     'nodes'
 )
 partitions = data_bag_item(
-    "slurm-#{node['slurm-wlm']['configs']['ClusterName']}",
+    "slurm-#{node['slurm-wlm']['config']['ClusterName']}",
     'partitions'
 )
 
@@ -19,6 +19,14 @@ template '/home/vagrant/slurm.conf' do
   source 'slurm.conf.erb'
   owner 'root'
   mode '0755'
-  variables(cluster_nodes: nodes, partitions: partitions)
+  variables(cluster_nodes: nodes, 
+            partitions: partitions,
+            config: node['slurm-wlm']['config'],
+            configdir: node['slurm-wlm']['files']['configdir'],
+            rundir: node['slurm-wlm']['files']['rundir'],
+            service: node['slurm-wlm']['files']['service'])
   action :create
 end
+
+# puts '------------'
+# puts node['slurm-wlm']['files']['job_checkpoint_dir']
