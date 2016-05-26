d220eac7b9807b7804334636e1359b427ef84cb2
iamericmaxwell <iamericmaxwell@gmail.com>
2016-05-25 17:03:21 -0700

    Added comment

--- recipes/pam.rb
+++ recipes/pam.rb
@@ -2,6 +2,7 @@ package 'libpam-slurm' do
   action :install
 end
 
+# this uses the resource from the line cookbook from supermarket
 append_if_no_line 'append line to common-account' do
   path '/etc/pam.d/common-account'
   line 'account required                        pam_slurm.so'
