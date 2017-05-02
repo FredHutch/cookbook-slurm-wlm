e7c9293d459695046f7b239fa0abb1bc58d4fd15
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-05-02 14:39:35 -0700

    removed dependency on line

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,5 @@ maintainer 'Scientific Computing - Fred Hutchinson Cancer Research Ctr.'
 maintainer_email 'scicomp@fredhutch.org'
 license 'Apache 2.0'
 description 'Installs and configures SchedMD Slurm Workload Manager'
-version '0.1.0'
+version '0.2.0'
 
-depends 'line', '~> 0.6.3'
