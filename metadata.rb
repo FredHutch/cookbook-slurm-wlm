8b10a0b0234c7b06d8f4407b7769a7d939856488
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-05-02 14:46:46 -0700

    Foodcritic fixes

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,10 @@ maintainer_email 'scicomp@fredhutch.org'
 license 'Apache 2.0'
 description 'Installs and configures SchedMD Slurm Workload Manager'
 version '0.2.0'
+chef_version '~> 12'
 
+issues_url 'https://github.com/FredHutch/cookbook-slurm-wlm/issues'
+source_url 'https://github.com/FredHutch/cookbook-slurm-wlm'
+
+supports 'ubuntu', '~> 12.04'
+supports 'ubuntu', '~> 14.04'
