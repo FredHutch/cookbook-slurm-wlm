f56aa8ad6ea4e8e89ae75a88695727ade81bc662
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-05-02 09:10:41 -0700

    Updated default recipe
     - installs the repository, configuration, and client

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,12 +5,5 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 include_recipe 'slurm-wlm::repo'
-# include_recipe 'slurm-wlm::munge'
-# include_recipe 'slurm-wlm::config'
-# include_recipe 'slurm-wlm::client'
-# include_recipe 'slurm-wlm::munge'
-include_recipe 'slurm-wlm::pam'
-# include_recipe 'slurm-wlm::slurmctld'
-# include_recipe 'slurm-wlm::conf'
-# include_recipe 'slurm-wlm::slurmd'
-# include_recipe 'slurm-wlm::slurmdbd'
+include_recipe 'slurm-wlm::config'
+include_recipe 'slurm-wlm::client'
