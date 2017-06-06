6cf873b33d739971961801b554a291f3a51ab205
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-06-06 14:12:24 -0700

    Update service configuration
     - correct service name
     - defer startup: just enable

--- recipes/slurmctld.rb
+++ recipes/slurmctld.rb
@@ -3,6 +3,6 @@ unless node['slurm-wlm']['packages']['manage'] == false
   package node['slurm-wlm']['packages']['slurmctld']
 end
 
-service node['slurm-wlm']['services']['slurmd'] do
-  action [:enable, :start]
+service node['slurm-wlm']['services']['slurmctld'] do
+  action [:enable]
 end
