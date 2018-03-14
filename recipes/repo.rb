d37f96d438d080ba1ed2126d03ec3cc87088cb94
Michael Gutteridge <michael.gutteridge@gmail.com>
2018-03-14 09:34:06 -0700

    Logic for defaulting distribution
    Primarily to support test kitchen installs with multiple platforms... if
    distribution isn't set in the attribute, use the lsb codename

--- recipes/repo.rb
+++ recipes/repo.rb
@@ -5,11 +5,15 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 unless node['slurm-wlm']['repository']['uri'].nil?
+  if node['slurm-wlm']['repository']['distribution'].nil?
+    distribution = node['lsb']['codename']
+  end
+
   apt_repository 'slurm-wlm' do
     uri node['slurm-wlm']['repository']['uri']
     components node['slurm-wlm']['repository']['components']
     arch node['slurm-wlm']['repository']['arch']
-    distribution node['slurm-wlm']['repository']['distribution']
+    distribution distribution
     key node['slurm-wlm']['repository']['key']
     keyserver node['slurm-wlm']['repository']['keyserver']
   end
