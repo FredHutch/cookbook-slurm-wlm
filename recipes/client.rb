73ffcfbb5e838bf476c83e3bc1cb80964255ebbf
Michael Gutteridge <michael.gutteridge@gmail.com>
2018-03-23 11:21:46 -0700

    Release 0.2.12 - package installs

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,9 +1,9 @@
 include_recipe 'slurm-wlm::munge'
 unless node['slurm-wlm']['packages']['manage'] == false
-  package node['slurm-wlm']['packages']['client'] do
-    version node['slurm-wlm']['packages']['version']
-  end
-  package node['slurm-wlm']['packages']['plugins'] do
+  package [
+    node['slurm-wlm']['packages']['client'],
+    node['slurm-wlm']['packages']['plugins']
+  ] do
     version node['slurm-wlm']['packages']['version']
   end
 end
