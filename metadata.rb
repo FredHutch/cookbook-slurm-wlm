99a7e3a8d57a0c725e8fce827686b2f6b28fd29b
Michael Gutteridge <michael.gutteridge@gmail.com>
2018-05-17 14:57:51 -0700

    Release 0.2.16

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Scientific Computing - Fred Hutchinson Cancer Research Ctr.'
 maintainer_email 'scicomp@fredhutch.org'
 license 'Apache 2.0'
 description 'Installs and configures SchedMD Slurm Workload Manager'
-version '0.2.15'
+version '0.2.16'
 chef_version '~> 12'
 
 issues_url 'https://github.com/FredHutch/cookbook-slurm-wlm/issues'
