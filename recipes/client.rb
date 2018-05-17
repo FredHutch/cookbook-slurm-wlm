17fa58aa78dad37e52413a232f0185d90e41affa
Michael Gutteridge <michael.gutteridge@gmail.com>
2018-05-17 14:51:45 -0700

    Update package resource name for client package installs

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,9 +1,16 @@
 include_recipe 'slurm-wlm::munge'
 unless node['slurm-wlm']['packages']['manage'] == false
-  package [
-    node['slurm-wlm']['packages']['client'],
-    node['slurm-wlm']['packages']['plugins']
-  ] do
-    version node['slurm-wlm']['packages']['version']
+  # This requires a string for the name as using edit or
+  # delete _resource functions don't seem to work when
+  # the package name is an array
+  package 'slurm-client-packages' do
+    package_name [
+      node['slurm-wlm']['packages']['client'],
+      node['slurm-wlm']['packages']['plugins']
+    ]
+    version [
+      node['slurm-wlm']['packages']['version'],
+      node['slurm-wlm']['packages']['version']
+    ]
   end
 end
