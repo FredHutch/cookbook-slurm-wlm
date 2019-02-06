45601bf7b5c2c6e31bc132ab4b64bea4a13438b3
Michael Gutteridge <michael.gutteridge@gmail.com>
2019-02-06 10:08:28 -0800

    Release 0.6.0
     - Add user and group management functions for the slurm user

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Scientific Computing - Fred Hutchinson Cancer Research Ctr.'
 maintainer_email 'scicomp@fredhutch.org'
 license 'Apache 2.0'
 description 'Installs and configures SchedMD Slurm Workload Manager'
-version '0.5.0'
+version '0.6.0'
 chef_version '~> 14.8.12'
 
 issues_url 'https://github.com/FredHutch/cookbook-slurm-wlm/issues'
