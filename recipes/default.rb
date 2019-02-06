55f9e3fd8bec8d75d577ac5ec93e24a7cdfbd4d3
Michael Gutteridge <michael.gutteridge@gmail.com>
2019-02-06 08:40:58 -0800

    Re-order recipe execution in default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,5 +5,5 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 include_recipe 'slurm-wlm::repo'
-include_recipe 'slurm-wlm::config'
 include_recipe 'slurm-wlm::client'
+include_recipe 'slurm-wlm::config'
